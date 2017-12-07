6784bf4bcf69c7c51ae5a675ca2e91af7b6e42dd
J-Fo-S <sherm_jonathan@hotmail.com>
2017-12-08 02:38:54 +0800

    no message

--- appscripts/sketch.js
+++ appscripts/sketch.js
@@ -1,49 +1,63 @@
-//src: http://creative-coding.decontextualize.com/video/
 
 var w = 640;
 var h = 480;
-var img;
-var increment=0;
-var ref = [];
-var px = [];
-
-function preload() {
-  img = loadImage("assets/P_20151101_081631.png");
-}
+var xOff = 0;
+var yOff = 0.5;
 
 function setup() {
+  //var contX = document.getElementById("frame0");
+  //console.log(contX);
+  canvas =createCanvas(windowWidth, windowHeight);
   w = windowWidth;
   h = windowHeight;
-  createCanvas(w, h);
-  //imageMode(CENTER);
-  img.resize(w, h);
-  img.loadPixels();
-  noStroke();
-  
-
+  background(255);
+  //frameRate(2);
 }
 function draw() {
-  //background(0, 10, 10, 1);
-  ratioX = Math.trunc(w/32);
+  ratioX = Math.trunc(w/64);
   ratioY = Math.trunc(h/24);
-  image(img, 0, 0); // comment this out to only show rect shapes below
-  //img.loadPixels(); // loads the pixels to get the color attributes of each XY position
-  for (var x = 0; x < img.width; x += ratioX) {
-     for (var y = 0; y < img.height; y += ratioY){
-      var pixVals = ((y*w)+x)*4; //SEEMS CONFUSING AT FIRST, but each pixel has 4 values (RGBA)
-      push();
-      translate(x, y);
-      rotate(x+increment*noise(x)*0.2);
-      //fill((img.pixels[pixVals+2]+increment)%img.pixels[pixVals+2], (img.pixels[pixVals+1]+increment)%img.pixels[pixVals+1], (img.pixels[pixVals]+increment)%img.pixels[pixVals]/8) ; // filling each stroke with inverted colors
-      //rect(0, 0, 2, 2);
-      //rotate(-4*(x+increment*noise(x)*0.2));
-      fill(img.pixels[pixVals], img.pixels[pixVals+1], img.pixels[pixVals+2], img.pixels[pixVals+2]); // filling each pixel as rect with rgb values 
-      quad(sin(noise(increment*0.001))*20, tan(noise(increment*0.002))*2, tan(increment*0.0011)*-2, cos(increment*0.003)*20, cos(noise(increment*0.001))*-20, sin(noise(increment*0.001))*20, cos(increment*0.0012)*20, sin(increment*0.033)*20);
-      pop();
-  
-  	}
+  var sc = random(1);
+  if(sc > 0.5){
+    for (var i = 0; i < w; i+=ratioX) {
+      var shift = noise(xOff)*w;
+      xOff = xOff + 0.001;
+      var x = random(w);
+      var y = random(h);
+      var xw = random(10);
+      var c = random(50);
+      strokeWeight(0);
+      stroke(xw-100);
+      fill(c%200+55, c%100+15);
+      rect(i, y, c, c);
+      for (var j = 0; j < h; j+=ratioY){
+      	push();
+        fill(c%25+205, x%25+205);
+        rect(2*x-shift, j, xw, c);
+        pop();
+      }
+   }
+ } else{
+    
+    for (var i = 0; i < h; i+=ratioX) {
+      var shift = noise(yOff)*w;
+      yOff = yOff + 0.001;
+      var x = random(w);
+      var y = random(h);
+      var xw = random(10);
+      var c = random(50);
+      strokeWeight(0);
+      stroke(xw-100);
+      fill(c%200+55, c%10+15);
+      rect(x, i, xw, c);
+      for (var j = 0; j < w; j+=ratioY){
+        push();
+        fill(c%25+225, x%25+225);
+        rect(j, 2*x-shift, xw, xw);
+        pop();
+      }
   }
-  increment++;
+  }
+  
 }
 
 function windowResized() {
